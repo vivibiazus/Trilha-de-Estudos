253343d8d96314dec3b5f4dee01fa9206a271687
Viviane <151794010+vivibiazus@users.noreply.github.com>
2025-11-23 21:55:51 -0300

    Create MediaSimplesEstrategia.py

---
+++ src/model/MediaSimplesEstrategia.py
@@ -0,0 +1,11 @@
+from .EstrategiaProgresso import EstrategiaProgresso
+
+class MediaSimplesEstrategia(EstrategiaProgresso):
+    def calcular(self, trilha):
+        cursos = trilha.cursos
+        if not cursos:
+            return 0.0
+        soma = 0.0
+        for c in cursos:
+            soma += c.progresso()
+        return soma / len(cursos)
